dbd867ce6a4d00d8dd6bb6a1bc65e6d348267782
Ayush Pathak <ayushpathak13022006@gmail.com>
2026-02-07 17:55:48 +0530

    Reduce video latency

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <div id="page1" className="h-screen w-full relative flex flex-col items-center justify-center text-center text-white p-4">
-      <video autoPlay loop muted src="/resturant video.mp4" className="absolute top-0 left-0 h-full w-full object-cover z-0 brightness-[1.1]"></video>
+      <video autoPlay loop muted preload="metadata" poster="/Hero_img.png" src="/resturant-video.mp4" className="absolute top-0 left-0 h-full w-full object-cover z-0 brightness-[1.1]"></video>
       <div className="relative z-10 flex flex-col items-center justify-center">
       <h1 className="text-5xl md:text-8xl font-black relative"
       data-text="Experience Luxury, Redefined.">Experience Luxury, Redefined.</h1>
